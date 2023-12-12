3f9bbdf91af1d524e621a55478029ffcbc03fe89
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-12 20:56:12 -0300

    popup component

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const renderContent = {
+    1: (
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>Hi, I am <span className='font-semibold'>João Marcelo</span>! 👋</h1>
+    ),
+    2: (
+        <h1>2</h1>
+    ),
+    3: (
+        <h1>3</h1>
+    ),
+    4: (
+        <h1>4</h1>
+    ),
+};
+
+const infoBox = ({ text, link, btnText }) => {
+    <div className='info-box'>
+        {text}
+    </div>
+}
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-unknown-property */
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import birdScene from '../assets/3d/bird.glb'
 import { useAnimations, useGLTF } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 
 const Bird = () => {
 
@@ -14,8 +14,8 @@ const Bird = () => {
       actions['Take 001'].play();
     }, []);
 
-    useFrame(({ clock, camera}) => {
-      birdRef.current.position.y += Math.sin(clock.elapsedTime) * 0.2 + 2;
+    useFrame(({ clock, camera }) => {
+      birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
 
       if (birdRef.current.position.x > camera.position.x + 10) {
         birdRef.current.rotation.y = Math.PI;
@@ -24,11 +24,11 @@ const Bird = () => {
       }
 
       if(birdRef.current.rotation.y === 0) {
-        birdRef.current.rotation.x += 0.01;
-        birdRef.current.rotation.z -= 0.01;
+        birdRef.current.position.x += 0.01;
+        birdRef.current.position.z -= 0.01;
       } else {
-        birdRef.current.rotation.x -= 0.01;
-        birdRef.current.rotation.z += 0.01;
+        birdRef.current.position.x -= 0.01;
+        birdRef.current.position.z += 0.01;
       }
     })
 
