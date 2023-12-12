67abb06ecded57a8088f504c37deff2c6b5c9312
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-12 10:29:28 -0300

    loader component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,18 @@ const Navbar = () => {
         <NavLink to='/' className={'w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'}>
             <p className='blue-gradient-text'>JD</p>
         </NavLink>
+        <div className='flex gap-3'>
         <nav className='flex text-lg gap-7 font-medium'>
             <NavLink to='/about' className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
                 About
             </NavLink>
         </nav>
+        <nav className='flex text-lg gap-7 font-medium'>
+            <NavLink to='/projects' className={({isActive}) => isActive ? 'text-blue-500' : 'text-black'}>
+                Projects
+            </NavLink>
+        </nav>
+        </div>
     </header>
   )
 }
