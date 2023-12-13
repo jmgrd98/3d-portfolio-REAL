dee8517289502385268c9a3fc5bfd23bae93b995
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-13 12:19:21 -0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm(
+    emailjs.send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
@@ -38,8 +38,16 @@ const Contact = () => {
           from_email: form.email,
           to_email: 'jmgrd98@gmail.com',
           message: form.message
-        }
-      )
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      ).then(() => {
+        setIsLoading(false);
+
+        setForm({name: '', email: '', message: ''});
+      }).catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+      })
   };
 
   return (
