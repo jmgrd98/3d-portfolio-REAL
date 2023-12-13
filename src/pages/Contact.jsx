41cc161ebe01b5a26002ec95790650517f838c47
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-12 21:08:07 -0300

    popup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = () => {
+
+  };
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in touch</h1>
+
+        <form className='w-full flex flex-col gap-7 mt-14'>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
   )
 }
 
