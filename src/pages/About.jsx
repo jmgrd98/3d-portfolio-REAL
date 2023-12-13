3b92299d5f7806bdc4a426b8a62d69ab4ec9dbf8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-13 15:52:33 -0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
@@ -15,7 +16,19 @@ const About = () => {
         <h3 className='subhead-text'>My Skills</h3>
 
         <div className='mt-16 flex flex-wrap gap-12'>
-          
+          {skills.map((skill) => (
+            <div key={skill.imageUrl} className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl'>
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                  />
+                </div>
+            </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
