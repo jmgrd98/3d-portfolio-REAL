63c10aabdb6140d84ce5c45e0fa75da5a2480196
Joao Dantas <jmgrd98@gmail.com>
2024-01-22 12:40:55 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
   esbuild: {
     target: 'es2015',
   },
